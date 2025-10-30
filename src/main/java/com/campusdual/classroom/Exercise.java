8ecd9ad4a94553aa4a52df4256da0dc75327e68c
Miriam Carballo Rodríguez <miriamcarballorodriguez@gmail.com>
2025-10-30 18:35:14 +0100

    Solved Exercise 23

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -5,13 +5,33 @@ import java.util.Queue;
 
 public class Exercise {
     public static Queue<String> createQueue() {
+        Queue<String> Surname = new LinkedList<>();
+
+        Surname.add("Smith");
+        Surname.add("Montessori");
+        Surname.add("Peralta");
+        Surname.add("House");
+
+        return Surname;
 
     }
+
     public static void printAndEmptyQueue(Queue<String> queue) {
+        System.out.println("Al inicio de la cola: " + queue.peek());
+
+        while (!queue.isEmpty()) {
+           String Surname = queue.poll();
+           System.out.println("Procesando y eliminando: " + Surname);
+        }
+
+        System.out.println("Cola vacía: " + queue.isEmpty());
+
+
 
     }
-
     public static void main(String[] args) {
+        Queue<String> surname = createQueue();
+        printAndEmptyQueue(surname);
 
     }
 
